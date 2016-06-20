2b6cf9aac838a2891f6e853e8ce0aa55079970d7
Mike Manturov <insolor@users.noreply.github.com>
2016-06-20 23:31:33 +0300

    Using open dialog

--- osv_cmp_gui.py
+++ osv_cmp_gui.py
@@ -9,10 +9,14 @@ from osv_cmp import load_osv_smeta, load_osv_1c
 
 class App(tk.Tk):
     def bt_pick_file1(self, event):
-        messagebox.showinfo('Title', 'Message')
+        self.filename1 = filedialog.askopenfilename(filetypes=[('Документ Excel', '*.xls')])
+        self.entry1.delete(0, tk.END)
+        self.entry1.insert(0, self.filename1)
 
     def bt_pick_file2(self, event):
-        pass
+        self.filename2 = filedialog.askopenfilename(filetypes=[('Документ Excel', '*.xls')])
+        self.entry2.delete(0, tk.END)
+        self.entry2.insert(0, self.filename2)
 
     def __init__(self):
         super().__init__()
@@ -23,22 +27,21 @@ class App(tk.Tk):
 
         self.entry1 = ttk.Entry(self, width=100)
         self.entry1.grid(column=2, row=1, sticky=tk.EW)
-
-
-        ttk.Button(self, text='Загрузить/проверить').grid(column=3, row=1)
         
-        ttk.Button(self, text='Выбрать файл 2').grid(column=1, row=2)
+        button = ttk.Button(self, text='Выбрать файл 2')
+        button.grid(column=1, row=2)
+        button.bind('<1>', self.bt_pick_file2)
         
         self.entry2 = ttk.Entry(self, width=100)
         self.entry2.grid(column=2, row=2, sticky=tk.EW)
+        
+        ttk.Button(self, text='Загрузить/\nперечитать').grid(column=3, row=1, rowspan=2, sticky=tk.NS)
 
-
-        ttk.Button(self, text='Загрузить/проверить').grid(column=3, row=2)
         ttk.Button(self, text='Сравнить').grid(column=4, row=1, rowspan=2, sticky=tk.NS)
 
         tk.Text().grid(column=1, row=4, columnspan=5, sticky=tk.EW)
-        ttk.Button(self, text='Очистить отчет').grid(column=1, row=5)
-        ttk.Button(self, text='Сохранить отчет').grid(column=2, row=5)
+        
+        ttk.Button(self, text='Сохранить отчет').grid(column=1, row=5)
 
 app = App()
 app.mainloop()
