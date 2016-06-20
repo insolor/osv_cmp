8084003e1019ddc0afbd19ca03258f1396a045ae
Mike Manturov <insolor@users.noreply.github.com>
2016-06-20 18:57:38 +0300

    Using ttk widgets

--- osv_cmp_gui.py
+++ osv_cmp_gui.py
@@ -16,27 +16,29 @@ class App(tk.Tk):
 
     def __init__(self):
         super().__init__()
-
-        self.entry1 = tk.Entry(self, width=100)
-        self.entry1.grid(column=1, row=1, sticky=tk.EW)
-
-        button = tk.Button(self, text='Выбрать файл 1')
-        button.grid(column=2, row=1)
+        
+        button = ttk.Button(self, text='Выбрать файл 1')
+        button.grid(column=1, row=1)
         button.bind('<1>', self.bt_pick_file1)
 
-        tk.Button(self, text='Загрузить/проверить').grid(column=3, row=1)
+        self.entry1 = ttk.Entry(self, width=100)
+        self.entry1.grid(column=2, row=1, sticky=tk.EW)
 
-        self.entry2 = tk.Entry(self, width=100)
-        self.entry2.grid(column=1, row=2, sticky=tk.EW)
 
-        tk.Button(self, text='Выбрать файл 2').grid(column=2, row=2)
-        tk.Button(self, text='Загрузить/проверить').grid(column=3, row=2)
-        tk.Button(self, text='Сравнить').grid(column=4, row=1, rowspan=2, sticky=tk.NS)
+        ttk.Button(self, text='Загрузить/проверить').grid(column=3, row=1)
+        
+        ttk.Button(self, text='Выбрать файл 2').grid(column=1, row=2)
+        
+        self.entry2 = ttk.Entry(self, width=100)
+        self.entry2.grid(column=2, row=2, sticky=tk.EW)
+
+
+        ttk.Button(self, text='Загрузить/проверить').grid(column=3, row=2)
+        ttk.Button(self, text='Сравнить').grid(column=4, row=1, rowspan=2, sticky=tk.NS)
 
         tk.Text().grid(column=1, row=4, columnspan=5, sticky=tk.EW)
-        tk.Button(self, text='Очистить отчет').grid(column=1, row=5)
-        tk.Button(self, text='Сохранить отчет').grid(column=2, row=5)
-
+        ttk.Button(self, text='Очистить отчет').grid(column=1, row=5)
+        ttk.Button(self, text='Сохранить отчет').grid(column=2, row=5)
 
 app = App()
 app.mainloop()
