9868f72252734d2726f566afae65de6d0e7bbe73
Mike Manturov <insolor@users.noreply.github.com>
2016-06-22 22:56:45 +0300

    Simplify check_format() code

--- osv_cmp.py
+++ osv_cmp.py
@@ -96,9 +96,7 @@ def load_osv_smeta(sheet: xlrd.sheet.Sheet):
 
 
 def check_format(sheet: xlrd.sheet.Sheet):
-    cell00 = sheet.row_values(0)[0]
-    cell10 = sheet.row_values(1)[0]
-    if cell00.startswith('Оборотно-сальдовая ведомость') or cell10.startswith('Оборотно-сальдовая ведомость'):
+    if any(sheet.row_values(i)[0].startswith('Оборотно-сальдовая ведомость') for i in range(2)):
         return '1c'
     elif cell10 == 'ОБОРОТНО-САЛЬДОВАЯ ВЕДОМОСТЬ':
         return 'Smeta'
